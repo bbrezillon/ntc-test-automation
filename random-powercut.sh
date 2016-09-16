07a93440f7606019ddda192bc15d5eb687f92213
Boris Brezillon <boris.brezillon@free-electrons.com>
2016-09-16 10:16:49 +0200

    random-powercut: make max and min time configurable and increase default values
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- random-powercut.sh
+++ random-powercut.sh
@@ -3,12 +3,22 @@
 RELAY=$1
 RELAY_IP=$2
 RELAY_PORT=$3
+MIN_TIME=$4
+MAX_TIME=$5
+
+if [ "x$MIN_TIME" = "x" ]; then
+	MIN_TIME=300
+fi
+
+if [ "x$MAX_TIME" = "x" ]; then
+	MAX_TIME=900
+fi
 
 command_relay.py $RELAY_IP $RELAY_PORT $RELAY on
 
 while [ true ]; do
 	echo "rnd = $RANDOM"
-	WAIT=$(((RANDOM%600)+60))
+	WAIT=$(((RANDOM%(MAX_TIME-MIN_TIME))+MIN_TIME))
 	echo "waiting $WAIT secs before power-cut"
 	sleep $WAIT
 	
